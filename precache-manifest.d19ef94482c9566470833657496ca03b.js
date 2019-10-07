53eeb235aa6568d91b0fea3358cf883e449278d4
devxa2602 <akshayrawate@gmail.com>
2019-10-07 14:13:47 +0000

    Updates

---
+++ precache-manifest.d19ef94482c9566470833657496ca03b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09d76bd81675e36dee2870a52ae01ea",
+    "url": "/movies-series-app/index.html"
+  },
+  {
+    "revision": "b26ae6681c1d4c973d98",
+    "url": "/movies-series-app/static/css/main.95c0fcfc.chunk.css"
+  },
+  {
+    "revision": "3fafcfec0c3197cb78a2",
+    "url": "/movies-series-app/static/js/2.8f1c32d3.chunk.js"
+  },
+  {
+    "revision": "b26ae6681c1d4c973d98",
+    "url": "/movies-series-app/static/js/main.14b91799.chunk.js"
+  },
+  {
+    "revision": "3d295bd6407e5f219211",
+    "url": "/movies-series-app/static/js/runtime-main.c3ae6e52.js"
+  }
+]);
